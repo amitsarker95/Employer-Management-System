64d47a4f8151fb2d0e180afa958df5c3935bbc23
amit <amitsarker95@gmail.com>
2025-05-20 22:58:32 +0600

    Make change in RegistrationSerializer, UserSignUpView and added JWT Authentications as well.

--- core/serializers.py
+++ core/serializers.py
@@ -1,18 +1,40 @@
 from rest_framework import serializers
+from django.conf import settings
 from .models import CustomUser
 
 
-class UserSignUpSerializer(serializers.Serializer):
-    
-    password = serializers.CharField(max_length=100, write_only=True)
-    
-    class Meta:
-        model = CustomUser
-        fields = ['email', 'full_name', 'password']
+class RegistrationSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
+    password2 = serializers.CharField(style={'input_type' : 'password'} ,write_only = True)
+
+    class Meta:
+
+        model = CustomUser
+        fields = ['full_name', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password' : {'write_only': True}
+        }
+
+    def save(self, *args, **kwargs):
+        
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        user = CustomUser.objects.filter(email= self.validated_data['email'])
+
+        if password != password2:
+            raise serializers.ValidationError({'error' : 'Password and confirm password are not the same'})
+        if user.exists():
+            raise serializers.ValidationError({'error' : 'This email already exists'})
+        
+        account = CustomUser.objects.create(
+            email=self.validated_data['email'],
+            full_name=self.validated_data['full_name']
+            )
+        account.set_password(password)
+        account.save()
+
+        return account
     
 
 class ProfileSerializer(serializers.ModelSerializer):
--- core/views.py
+++ core/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import generics, permissions
+from rest_framework import permissions
 from rest_framework.views import APIView
-from .serializers import UserSignUpSerializer, ProfileSerializer
+from rest_framework import status
+from .serializers import RegistrationSerializer, ProfileSerializer
 
 
-class UserSignUpView(generics.CreateAPIView):
-    serializer_class = UserSignUpSerializer
+
+class UserSignUpView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
+    
 class UserProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
